c8d483edf988096dae5d7fa783dd34d037fa253a
AFungo <augustofungo@gmail.com>
2023-04-14 11:39:29 -0300

    add test cases

--- engine/test/problems/practica_1/test_n_puzzle.py
+++ engine/test/problems/practica_1/test_n_puzzle.py
@@ -1,20 +1,40 @@
 import pytest
 
+from engine.algorithms.informed.astar_searh import astar_search
 from engine.algorithms.uninformed.breadth_first_graph_search import breadth_first_graph_search
-from engine.problems.practica_1.n_puzzle import NPuzzle, NPuzzleState
+from engine.problems.practica_1.n_puzzle import NPuzzle, NPuzzleState, manhattan_heuristic
 
 
 @pytest.fixture
 def params():
     params = {}
-    params['initial_state'] = NPuzzleState((1, 2, 3,
-                                            0, 5, 4,
-                                            7, 8, 6))
-    params['n_puzzle_problem'] = NPuzzle(params['initial_state'], 3)
+    params['goal_initial_state'] = NPuzzleState((1, 2, 3,
+                                                 4, 5, 6,
+                                                 7, 8, 0), 3)
+    params['medium_initial_state'] = NPuzzleState((4, 1, 2,
+                                                   7, 5, 3,
+                                                   8, 0, 6), 3)
+    params['advanced_initial_state'] = NPuzzleState((1, 2, 3,
+                                                     4, 0, 6,
+                                                     7, 8, 5), 3)
+    params['n_puzzle_problem'] = NPuzzle(params['goal_initial_state'], 3)
     return params
 
 
 def test_n_puzzle_breadth_first_graph_search(params):
     solution = breadth_first_graph_search(params['n_puzzle_problem']).solution()
+    expected = []
+    assert expected == solution
+
+
+def test_n_puzzle_astar_search(params):
+    solution = astar_search(params['n_puzzle_problem']).solution()
+    expected = []
+    assert expected == solution
+
+
+def test_n_puzzle_astar_search_manhattan_heuristic(params):
+    params['n_puzzle_problem'] = NPuzzle(params['advanced_initial_state'], 3)
+    solution = astar_search(params['n_puzzle_problem'], manhattan_heuristic).solution()
     expected = []
     assert expected == solution
