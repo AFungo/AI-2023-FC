f18bf62a3d30a186ddf0b11b80e6d27167b3b2b7
AgustinCalderon <agustincalderon058@gmail.com>
2023-04-18 14:39:14 -0300

    Fixed actions for nqueens

--- engine/problems/practica_1/n_queeens.py
+++ engine/problems/practica_1/n_queeens.py
@@ -3,10 +3,13 @@ from engine.problems.abstractproblem import State, Actions, MyProblem
 
 class NQueensProblem(MyProblem):
 
-    def __int__(self, n):
+    def __init__(self, n):
         initial_state = NQueensState(n, tuple([-1] * n))
-        actions_list = [Place_A_Queen_In_Board()]
-        super().__init__(initial_state, actions_list, None)
+        actions_list = None
+        for i in range(n):
+            for j in range(n):
+                actions_list = [Place_A_Queen_In_Board(i, j)]
+        super().__init__(initial_state, actions_list)
 
     def goal_test(self, state):
         """ Given a state, return True if state is a goal state or False, otherwise """
@@ -41,16 +44,22 @@ class NQueensState(State):
 
 class Place_A_Queen_In_Board(Actions):
 
-    def is_enable(self, state):
-        col = state.index(-1)
-        for row in range(state.N):
-            if not self.conflicted(state, row, col):
-                return True
-        return False
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
 
-    def conflicted(self, board, row, col):
+    def is_enable(self, state):
+        return not self.conflicted(state, self.row, self.col)
+
+        # col = state.board.index(-1)
+        # for row in range(state.n):
+        #     if not self.conflicted(state, row, col):
+        #         return True
+        # return False
+
+    def conflicted(self, state, row, col):
         """Would placing a queen at (row, col) conflict with anything?"""
-        return any(self.conflict(row, col, board[c], c)
+        return any(self.conflict(row, col, state.board[c], c)
                    for c in range(col))
 
     @staticmethod
@@ -63,9 +72,7 @@ class Place_A_Queen_In_Board(Actions):
 
     def execute(self, state):
         """In the leftmost empty column, try all non-conflicting rows."""
-        if state[-1] != -1:
-            return []  # All columns filled; no successors
-        else:
-            col = state.index(-1)
-            return [row for row in range(state.N)
-                    if not self.conflicted(state, row, col)]
+        if self.is_enable(state):
+            new_board = list(state.board)
+            new_board[self.col] = self.r            return NQueensState(state.n, tuple(new_board))
+        return ()
--- engine/test/problems/practica_1/test_n_queens.py
+++ engine/test/problems/practica_1/test_n_queens.py
@@ -1,23 +1,36 @@
 import pytest
 
 from engine.algorithms.uninformed.breadth_first_graph_search import breadth_first_graph_search
-from engine.problems.practica_1.n_queeens import NQueensState, NQueensProblem
-from engine.problems.practica_1.n_puzzle import NPuzzle, NPuzzleState, manhattan_heuristic
+from engine.problems.practica_1.n_queeens import NQueensState, NQueensProblem, Place_A_Queen_In_Board
 
 
 def test_state_is_goal():
-    eight_queen = NQueensProblem(8, None)
+    eight_queen = NQueensProblem(8)
     state = NQueensState(8, (7, 3, 0, 2, 5, 1, 6, 4))
     assert eight_queen.goal_test(state)
 
 
 def test_state_is_not_goal():
-    eight_queen = NQueensProblem(8, None)
+    eight_queen = NQueensProblem(8)
     state = NQueensState(8, (1, 2, 3, 2, 5, 1, 6, 4))
     assert not eight_queen.goal_test(state)
 
 
 def test_eight_queen_breadth_first_graph_search():
-    eight_queen = NQueensProblem(8, None)
+    eight_queen = NQueensProblem(4)
     solution = breadth_first_graph_search(eight_queen).solution()
-    # assert [] == solution
+    assert () == solution
+
+
+def test_action_execute_():
+    eight_queen = NQueensProblem(8)
+    state = NQueensState(8, tuple([-1] * 8))
+    action = Place_A_Queen_In_Board(0, 0)
+
+    aux = action.execute(state)
+    assert True
+
+
+# def test_eight_queen_depth_first_search():
+#     eight_queen = NQueensProblem(8)
+#     solution = DepthFirstSearch.search(eight_queen).solution()
