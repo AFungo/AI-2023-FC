c455540483f7d25b8b7a74fc441aa15a6579642a
AFungo <augustofungo@gmail.com>
2023-04-24 16:04:33 -0300

    refactor breadth first graph search

--- engine/algorithms/uninformed/breadth_first_graph_search.py
+++ engine/algorithms/uninformed/breadth_first_graph_search.py
@@ -2,23 +2,27 @@ from engine.node import Node
 from collections import deque
 
 
-def breadth_first_graph_search(problem):
-    """[Figure 3.11]
-    Note that this function can be implemented in a
-    single line as below:
-    return graph_search(problem, FIFOQueue())
-    """
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = deque([node])
-    explored = set()
-    while frontier:
-        node = frontier.popleft()
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                if problem.goal_test(child.state):
-                    return child
-                frontier.append(child)
-    return None
+class BreadthFirstGraphSearch:
+    def __init__(self, problem):
+        self.problem = problem
+
+    def search(self):
+        """[Figure 3.11]
+        Note that this function can be implemented in a
+        single line as below:
+        return graph_search(problem, FIFOQueue())
+        """
+        node = Node(self.problem.initial)
+        if self.problem.goal_test(node.state):
+            return node
+        frontier = deque([node])
+        explored = set()
+        while frontier:
+            node = frontier.popleft()
+            explored.add(node.state)
+            for child in node.expand(self.problem):
+                if child.state not in explored and child not in frontier:
+                    if self.problem.goal_test(child.state):
+                        return child
+                    frontier.append(child)
+        return None
