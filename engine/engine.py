cc209033dc4b6cd4caa3b9712f04ce0604cb9f4b
AgustinCalderon <agustincalderon058@gmail.com>
2023-04-24 17:01:58 -0300

    Fixed nqueens tests

--- engine/engine.py
+++ engine/engine.py
@@ -1,5 +1,7 @@
 from enum import Enum, auto
 
+from engine.algorithms.informed.astar_search import AstarSearch
+from engine.algorithms.informed.best_first_graph_search import BestFirstGraphSearch
 from engine.algorithms.uninformed.breadth_first_search import BreadthFirstSearch
 from engine.algorithms.uninformed.depth_first_search import DepthFirstSearch
 from engine.problems.practica_1.missionary_and_cannibals_problem import MissionariesAndCannibalsProblem
@@ -46,11 +48,11 @@ class AlgorithmFactory:
             return self.create_uninformed_algorithm(self.algorithm)
         Exception("Algorithm type not supported")
 
-    def create_informed_algorithm(self, algorithm, heuristic):
+    def create_informed_algorithm(self, algorithm, problem, heuristic, display):
         if algorithm == InformedAlgorithms.ASTAR_SEARCH:
-            return AStarSearch(heuristic)
+            return AstarSearch(problem, heuristic, display)
         elif algorithm == InformedAlgorithms.BEST_FIRST_GRAPH_SEARCH:
-            return BestFirstSearch(heuristic)
+            return BestFirstGraphSearch(problem, heuristic, display)
 
     def create_uninformed_algorithm(self, algorithm):
         if algorithm == UninformedAlgorithms.BREADTH_FIRST_SEARCH:
