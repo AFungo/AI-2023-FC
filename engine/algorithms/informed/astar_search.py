9d3e6c5eb8764ad2d31b06e993ba20925bdfd150
AFungo <augustofungo@gmail.com>
2023-04-25 18:26:36 -0300

    refactor astar

--- engine/algorithms/informed/astar_search.py
+++ engine/algorithms/informed/astar_search.py
@@ -9,8 +9,5 @@ class AstarSearch:
         self.h = h
 
     def search(self):
-        """A* search is best-first graph search with f(n) = g(n)+h(n).
-        You need to specify the h function when you call astar_search, or
-        else in your Problem subclass."""
         self.h = memoize(self.h or self.problem.h, 'h')
         return BestFirstGraphSearch(self.problem, lambda n: n.path_cost + self.h(n)).search()
