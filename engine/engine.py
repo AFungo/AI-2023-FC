ce96102ad4f9cf99aaebf916edc638d9b9f8799a
AgustinCalderon <agustincalderon058@gmail.com>
2023-04-28 20:37:02 -0300

    Added new factory for heuristics

--- engine/engine.py
+++ engine/engine.py
@@ -11,18 +11,21 @@ from engine.problems.practica_1.missionary_and_cannibals_problem import Missiona
 from engine.problems.practica_1.n_puzzle import NPuzzle
 from engine.problems.practica_1.n_queeens import NQueensProblem
 from engine.problems.practica_1.romania_map import RomaniaMap
+from engine.problems.practica_1.n_puzzle import NPuzzleHeuristics
+from engine.problems.practica_1.n_queeens import NQueensHeuristics
+from engine.problems.practica_1.romania_map import RomaniaMapHeuristics
 from engine.problems.problem import Problem
 from engine.algorithms.search_algorithm import SearchAlgorithm
 
 
 class Engine:
     def __init__(self, problem, algorithm, initial_state, goal_state=None, heuristic=None, algorithm_params=None):
-        self.problem = ProblemFactory.create(problem, initial_state, goal_state)
-        algorithm_factory = AlgorithmFactory(algorithm, problem, heuristic=heuristic, params=algorithm_params)
+        self.problem = ProblemFactory(problem, initial_state, goal_state).create
+        self.heuristic = HeuristicFactory(problem, heuristic).create()
+        algorithm_factory = AlgorithmFactory(algorithm, problem, heuristic, params=algorithm_params)
         self.algorithm = algorithm_factory.create()
         self.initial_state = initial_state
         self.goal_state = goal_state
-        self.heuristic = heuristic
 
     def solve(self):
         node_solution = self.algorithm.search()
@@ -31,16 +34,47 @@ class Engine:
 
 class ProblemFactory:
 
-    def create(self, problem, initial_state, goal_state=None):
+    def __init__(self, problem, initial_state, goal_state=None):
+        self.problem = problem
+        self.initial_state = initial_state
+        self.goal_state = goal_state
 
-        if problem == Problems.ROMANIA_MAP:
-            return RomaniaMap(initial_state, goal_state)
-        elif problem == Problems.N_PUZZLE:
-            return NPuzzle(initial_state, goal_state)
-        elif problem == Problems.N_QUEENS:
-            return NQueensProblem(initial_state)
-        elif problem == Problems.CANNIBALS_AND_MISSIONARIES:
-            return MissionariesAndCannibalsProblem(initial_state)
+    def create(self):
+        if self.problem == Problems.ROMANIA_MAP:
+            return RomaniaMap(self.initial_state, self.goal_state)
+        elif self.problem == Problems.N_PUZZLE:
+            return NPuzzle(self.initial_state, self.goal_state)
+        elif self.problem == Problems.N_QUEENS:
+            return NQueensProblem(self.initial_state)
+        elif self.problem == Problems.CANNIBALS_AND_MISSIONARIES:
+            return MissionariesAndCannibalsProblem(self.initial_state)
+        Exception("Problem type not supported")
+
+
+class HeuristicFactory:
+
+    def __init__(self, problem, heuristic):
+        self.problem = problem
+        self.heuristic = heuristic
+
+    def create(self):
+        if self.heuristic is None:
+            return None
+
+        if self.problem == Problems.N_PUZZLE:
+            if self.heuristic == Heuristic.MANHATTAN:
+                return NPuzzleHeuristics().manhattan_heuristic
+            elif self.heuristic == Heuristic.MISPLACED_NUMBERS:
+                return NPuzzleHeuristics().misplaced_numbers
+            elif self.heuristic == Heuristic.GASCHNIG:
+                return NPuzzleHeuristics().gaschnig
+        elif self.problem == Problems.N_QUEENS:
+            if self.heuristic == Heuristic.UNATTACHED_SQUARES:
+                return NQueensHeuristics.unattacked_squares
+        elif self.problem == Problems.ROMANIA_MAP:
+            if self.heuristic == Heuristic.STRAIGTH_LINE_DISTANCE:
+                return RomaniaMapHeuristics.straigth_line_distance
+        Exception("Problem or Heuristic type not supported")
 
 
 class AlgorithmFactory:
@@ -89,6 +123,14 @@ class Problems(Enum):
     N_PUZZLE = auto()
     N_QUEENS = auto()
     CANNIBALS_AND_MISSIONARIES = auto()
+
+
+class Heuristic(Enum):
+    UNATTACHED_SQUARES = auto()
+    MANHATTAN = auto()
+    MISPLACED_NUMBERS = auto()
+    GASCHNIG = auto()
+    STRAIGTH_LINE_DISTANCE = auto()
 
 
 class AlgorithmsType(Enum):
