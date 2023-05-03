3a653527c05e76f7b85ec1eb6aa8adba129ed48f
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-02 22:49:52 -0300

    Scrip

---
+++ main/generated_problem.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+filename="./cfg_files/n_puzzle.csv"
+
+exec 3< "$filename"
+# shellcheck disable=SC2034
+read -r header_line <&3
+
+while IFS=',' read -r -u 3 result
+do
+      python3 n_puzzle.py "$result"
+done
+
+exec 3<&-
--- run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pytest engine/test/problems/practica_1/test_n_queens.py -k test_eight_queen_breadth_first_graph_search > result.txt 
-#pid=$!
-#sleep 60
-#if ps -p $pid > /dev/null; then
-#    kill $pid
-#fi
