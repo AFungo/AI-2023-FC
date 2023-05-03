ea376be0eeea4d41054d92dfd2fb64201c1705c7
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-03 11:41:55 -0300

    Update scrip

--- generated_problem.sh
+++ generated_problem.sh
@@ -8,17 +8,14 @@ exec 3< "$filename"
 # shellcheck disable=SC2034
 read -r header_line <&3
 
+# shellcheck disable=SC1073
 while IFS=',' read -r -u 3 result
 do
-      python3 ./main/n_puzzle.py "$result" &
-      PID=$!
-      sleep 600
-      if ps -p $pid > /dev/null; then
-        echo "Problem no finish"
-	      kill $PID
-      else  
-        echo "Problem finish"
-	      kill $PID
+      timeout -s SIGKILL 10s python3 ./main/n_puzzle.py "$result"
+      if [ $? -eq 0 ]; then
+        echo "Problem finish" >> log.txt
+      else
+        echo "Problem no finish" >> log.txt
       fi
 done
 
