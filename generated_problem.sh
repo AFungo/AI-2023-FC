c01a2846ccbde7534f8944a0c6bd81b42736710a
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-02 23:45:46 -0300

    Update scrip

--- generated_problem.sh
+++ generated_problem.sh
@@ -2,7 +2,6 @@
 # shellcheck disable=SC2155
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-
 filename="./main/cfg_files/n_puzzle.csv"
 
 exec 3< "$filename"
@@ -11,9 +10,16 @@ read -r header_line <&3
 
 while IFS=',' read -r -u 3 result
 do
-#      echo "$result"
-#      python -c "from engine.engine import *"
-      python3 ./main/n_puzzle.py "$result"
+      python3 ./main/n_puzzle.py "$result" &
+      PID=$!
+      timeout 10m || wait $PID
+      # shellcheck disable=SC2181
+      if [ $? -eq 0 ]; then
+        echo "Problem result"
+      else
+        echo "Problem no result"
+        kill $PID
+      fi
 done
 
 exec 3<&-
