dfee89a0d55a2bad06812d517378c762a8f1d6c9
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-08 11:28:25 -0300

    Added name problem

--- engine/problems/abstract_problem.py
+++ engine/problems/abstract_problem.py
@@ -81,6 +81,7 @@ class AbstractProblem(Problems):
 class CountNodes(Problems):
 
     def __init__(self, problem):
+        self.name_problem = problem.__class__.__name__
         self.problem = problem
         self.explored_node = 0
         self.generated_nodes = 0
