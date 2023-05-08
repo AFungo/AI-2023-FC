635c14e3343a328109da3d84cc5d956b2b6df33b
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-08 11:49:32 -0300

    update scrip log errors

--- generated_n_queen_problems.sh
+++ generated_n_queen_problems.sh
@@ -3,9 +3,7 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 filename="./main/cfg_files/n_queens.csv"
-touch "log.txt"
-#touch "log_errors.txt"
-#echo "" > log_errors.txt
+
 exec 3< "$filename"
 # shellcheck disable=SC2034
 read -r header_line <&3
@@ -13,15 +11,13 @@ read -r header_line <&3
 # shellcheck disable=SC1073
 while IFS=',' read -r -u 3 result
 do
-      # echo "$result" >> log_errors.txt
-      # shellcheck disable=SC2028
-      # echo -e "\n" >> log_errors.txt
+
       timeout -s SIGKILL 10s python3 ./main/n_queens.py "$result"
       # shellcheck disable=SC2181
       if [ $? -eq 0 ]; then
-        echo "Problem finish $result" >> log.txt
+        echo "Problem finish $result" >> n_queens_metrics/log.txt
       else
-        echo "Problem no finish $result" >> log.txt
+        echo "Problem no finish $result" >> n_queens_metrics/log.txt
       fi
 done
 exec 3<&-
