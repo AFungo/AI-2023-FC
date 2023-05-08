54bd4cc4545a9e3958da1d83d4ab0a9655f14d0f
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-08 19:54:41 -0300

    Update scrips

--- generated_n_puzzle_problems.sh
+++ generated_n_puzzle_problems.sh
@@ -3,9 +3,7 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 filename="./main/cfg_files/n_puzzle_new.csv"
-touch "n_puzzle_metrics/python_log.txt"
-touch "n_puzzle_metrics/log_errors.txt"
-echo "" > n_puzzle_metrics/log_errors.txt
+
 exec 3< "$filename"
 # shellcheck disable=SC2034
 read -r header_line <&3
@@ -13,16 +11,13 @@ read -r header_line <&3
 # shellcheck disable=SC1073
 while IFS=',' read -r -u 3 result
 do
-      echo "$result" >> n_puzzle_metrics/log_errors.txt
-      # shellcheck disable=SC2028
-      echo -e "\n" >> n_puzzle_metrics/log_errors.txt
       timeout -s SIGKILL 10s python3 ./main/n_puzzle.py "$result" >> n_puzzle_metrics/python_log.txt 2>> n_puzzle_metrics/log_errors.txt
       # shellcheck disable=SC2181
       if [ $? -eq 0 ]; then
         echo "Problem finish $result" >> n_puzzle_metrics/log.txt
       else
+        python3 ./main/n_puzzle_not_goal.py "$result" >> n_puzzle_metrics/python_log.txt 2>> n_puzzle_metrics/log_errors.txt
         echo "Problem no finish $result" >> n_puzzle_metrics/log.txt
-        python3 ./main/n_puzzle_not_goal.py "$result"
       fi
 done
 
--- generated_n_queen_problems.sh
+++ generated_n_queen_problems.sh
@@ -3,7 +3,6 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 filename="./main/cfg_files/n_queens.csv"
-echo "" > n_queens_metrics/log_errors.txt
 exec 3< "$filename"
 # shellcheck disable=SC2034
 read -r header_line <&3
@@ -11,7 +10,6 @@ read -r header_line <&3
 # shellcheck disable=SC1073
 while IFS=',' read -r -u 3 result
 do
-
       timeout -s SIGKILL 10s python3 ./main/n_queens.py "$result" >> n_queens_metrics/python_log.txt 2>> n_queens_metrics/log_errors.txt
       # shellcheck disable=SC2181
       if [ $? -eq 0 ]; then
