5e40ac160bb1e31cffe6379fb1b5195a9f1979c5
AgustinCalderon <agustincalderon058@gmail.com>
2023-05-08 16:20:52 -0300

    Update scrip to log error and states not goals

--- generated_n_puzzle_problems.sh
+++ generated_n_puzzle_problems.sh
@@ -3,9 +3,6 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 filename="./main/cfg_files/n_puzzle_new.csv"
-touch "log.txt"
-touch "log_errors.txt"
-echo "" > log_errors.txt
 exec 3< "$filename"
 # shellcheck disable=SC2034
 read -r header_line <&3
@@ -16,12 +13,12 @@ do
       echo "$result" >> log_errors.txt
       # shellcheck disable=SC2028
       echo -e "\n" >> log_errors.txt
-      timeout -s SIGKILL 10s python3 ./main/n_puzzle.py "$result" >> python_log.txt 2>> log_errors.txt
+      timeout -s SIGKILL 10s python3 ./main/n_puzzle.py "$result" >> n_puzzle_metrics/python_log.txt 2>> n_puzzle_metrics/log_errors.txt
       # shellcheck disable=SC2181
       if [ $? -eq 0 ]; then
-        echo "Problem finish $result" >> log.txt
+        echo "Problem finish $result" >> n_puzzle_metrics/log.txt
       else
-        echo "Problem no finish $result" >> log.txt
+        echo "Problem no finish $result" >> n_puzzle_metrics/log.txt
       fi
 done
 
